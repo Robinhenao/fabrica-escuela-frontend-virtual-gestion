c7c37e46197456c47fb8477d11a813755be94533
Robinson Henao <robinson.henao@udea.edu.co>
2024-05-25 14:53:21 -0500

    fix url

--- app/api/gestion/gestion.ts
+++ app/api/gestion/gestion.ts
@@ -20,7 +20,7 @@ export interface Flight {
   porcentajeImpuestos: number
 }
 
-const API_VUELOS_URL = "http://localhost:8080/api/v1/vuelos"
+const API_VUELOS_URL = "https://fabrica-backend-virtual-production.up.railway.app/"
 
 export const fetchAllFlights = (page: number, size: number): Promise<FlightsResponse> => {
   return axios.get<FlightsResponse>(`${API_VUELOS_URL}?page=${page}&size=${size}`).then(({ data }) => data)
